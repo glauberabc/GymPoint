17e9a3af66b491e79e5238d7efb0787e8494c2c3
glauber <glauberabc@gmail.com>
2019-10-21 16:52:00 -0300

    Alteraçao na senha do database

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: 'docker',
+  password: 'c3e3e6298c626272b9f5210ffced152ab724ff9d',
   database: 'gympoint',
   define: {
     timestamp: true,
